b3ddea8efc64af3ac56264daeff2b1b07ed3524c
Caua-Pablo <cauapxd1@gmail.com>
2024-08-26 23:50:06 -0300

    Realizando ajustes

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
-from screens.home_screen import HomeScreen
-from screens.add_password import AddPasswordScreen
-
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-
-
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
-
-# Defina o tamanho da janela para simular um dispositivo móvel
-Window.size = (360, 640)  # Dimensões típicas para um dispositivo móvel em modo retrato
+from kivy.lang import Builder
 
 
-
-class ScreenManagement(ScreenManager):
+# Definir as telas
+class HomeScreen(Screen):
     pass
 
-class PasswordManagerApp(App):
+class AddPasswordScreen(Screen):
+    pass
+
+class SegundaTela(Screen):
+    pass
+
+class MyScreenManager(ScreenManager):
+    pass
+
+class MyApp(App):
     def build(self):
-        sm = ScreenManagement()
+        # Definir a resolução e a cor de fundo
+        Window.size = (360, 640)
+        Window.clearcolor = (19/255, 33/255, 69/255, 1)  # Fundo azul
+        Builder.load_file('home_screen.kv')
+
+
+
+        
+
+        # Instanciar o ScreenManager
+        sm = MyScreenManager()
+
+        # Adicionar as telas ao ScreenManager
+        sm = MyScreenManager()
         sm.add_widget(HomeScreen(name='home'))
         sm.add_widget(AddPasswordScreen(name='add_password'))
+        sm.add_widget(SegundaTela(name='segunda_tela'))
         return sm
 
 if __name__ == '__main__':
-    PasswordManagerApp().run()
+    MyApp().run()
 
 
+
+
+       
--- screens/home_screen.py
+++ screens/home_screen.py
@@ -1,4 +1,25 @@
-from kivy.uix.screenmanager import Screen
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 class HomeScreen(Screen):
     pass
+
+class AddPasswordScreen(Screen):
+    pass
+
+class SegundaTela(Screen):
+    pass
+
+
+
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(HomeScreen(name='home'))
+        sm.add_widget(AddPasswordScreen(name='segunda_tela'))
+        sm.add_widget(AddPasswordScreen(name='add_password'))
+
+        return sm
+
+if __name__ == '__main__':
+    MyApp().run()
